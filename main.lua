c1f74caf0aaa81b15cd6e1054737956ba4676d99
Xkeeper <xkeeper@gmail.com>
2016-05-11 21:26:00 -0700

    Made clock feature (new branch)
    new font, new code

--- main.lua
+++ main.lua
@@ -17,7 +17,7 @@
 		love.graphics.setDefaultFilter("nearest", "nearest", 1)
 
 		fonts		= {
-			bignumbers	= love.graphics.newImageFont("numberfont-2x.png", "0123456789 .x-:"),
+			bignumbers	= love.graphics.newImageFont("numberfont-alt.png", "0123456789 .x-:"),
 			helptext	= love.graphics.newFont(12)
 			}
 
@@ -35,7 +35,7 @@
 			{ time =     0, sound = high },
 		}
 
-		currentMode	= modes.set
+		currentMode	= modes.timer
 
 	end
 
@@ -53,110 +53,18 @@
 	modes	= {}
 	modes.timer	= {}
 	modes.timer.draw	= function()
-		local localTimer	= globalTime - targetTime
 
-
-		local displayTimer	= math.min(100 * 60 - 1, math.floor(math.abs(localTimer)))
-
-		if not timerActive then
-			localTimer		= 100 * 60 - 1
-			displayTimer	= setTime
-			beep			= false
-		end
-
-		if beeps[beep] and beeps[beep].time < localTimer then
-			beeps[beep].sound:play()
-			beep = beep + 1
-		end
-
-		if not timerActive then
-			love.graphics.setBackgroundColor(40, 40, 40)
-			love.graphics.setColor(180, 180, 180)
-		
-		elseif localTimer < 0 then
-			love.graphics.setBackgroundColor(30, 30, 70)
-			love.graphics.setColor(255, 255, 255)
-
-		else
-			love.graphics.setBackgroundColor(50, 10, 10)
-			love.graphics.setColor(255, 120, 120)
-
-		end
-
-		local displayM	= math.floor(displayTimer / 60)
-		local displayS	= displayTimer % 60
-
-		love.graphics.print(string.format("%02d:%02d", displayM, displayS), textX, textY, 0, scale, scale)
-		displayHelpText(defualtHelpText .. "space: start/stop - enter: change setting")
-
-	end
-
-	modes.timer.keypressed	= function (key, rpt)
-		if key == " " then
-			targetTime		= globalTime + setTime
-			timerActive		= not timerActive
-			beep	= 1
-			return
-
-		elseif key == "return" then
-			timerActive		= false
-			currentMode		= modes.set
-
-		end
-
-	end
-
-
-	modes.set	= {}
-	modes.set.draw	= function()
-
-		love.graphics.setBackgroundColor(50, 70, 50)
+		love.graphics.setBackgroundColor(30, 30, 70)
 		love.graphics.setColor(255, 255, 255)
 
-		local disp	= string.gsub(string.format("%4d", setTimeDisplay), " ", "-")
-		if setTimeDisplay == 0 then
-			disp	= "----"
-		end
-		local disp1	= disp:sub(1, 2)
-		local disp2	= disp:sub(3, 4)
+		local currentTime	= os.date("*t")
 
-		love.graphics.print(string.format("%2s:%2s", disp1, disp2), textX, textY, 0, scale, scale)
-
-		displayHelpText(defualtHelpText .. "enter: save - 0-9, backspace: change time setting")
+		love.graphics.print(string.format("%02d:%02d:%02d", currentTime.hour, currentTime.min, currentTime.sec), textX, textY, 0, scale, scale)
 
 	end
 
-	do
-		local keymap	= {}
-		for i = 0, 9 do
-			keymap[tostring(i)]			= i
-			keymap["kp"..tostring(i)]	= i
-		end
+	modes.timer.keypressed	= function (key, rpt) end
 
-		modes.set.keypressed	= function (key, rpt)
-			if keymap[key] then
-				setTimeDisplay	= math.min(9959, setTimeDisplay * 10 + keymap[key])
-				return
-
-			elseif key == "backspace" then
-				setTimeDisplay	= math.floor(setTimeDisplay / 10)
-				return
-
-			elseif key == "return" then
-				local setS	= setTimeDisplay % 100
-				local setM	= math.floor(setTimeDisplay / 100)
-				if setS >= 60 then
-					setM	= setM + 1
-					setS	= setS - 60
-				end
-
-				setTime		= setM * 60 + setS
-				
-				currentMode	= modes.timer
-			end
-		end
-
-	end
 
 
 
@@ -205,7 +113,7 @@
 	function rescaleFont(w, h)
 		-- Get font required width/height
 		local font			= love.graphics.getFont()
-		local fontWidth		= font:getWidth("00:00");
+		local fontWidth		= font:getWidth("00:00:00");
 		local fontHeight	= font:getHeight();
 
 		-- Available size for font
